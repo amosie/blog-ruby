e23928477cd9034b023f8162e6dcafd51e25cb8e
amosie <amosie@github.com>
2015-12-14 17:34:16 +0000

    delete and add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, except: [:index, :new, :create]
+
   def index
     @posts = Post.all
   end
@@ -7,9 +9,13 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def edit
@@ -19,5 +25,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :pull_quote)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
