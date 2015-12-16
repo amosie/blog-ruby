52bae1a00bee8c3b2f83d4e056513fda2d11c477
amosie <amosie@github.com>
2015-12-16 12:57:42 +0000

    move flash message to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_warning
+    flash.each do |key, value|
+      content_tag :div, :class => key do
+        content_tag :p, value
+      end
+    end
+  end
 end
